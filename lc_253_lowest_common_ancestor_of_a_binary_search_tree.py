a2b24e469d2ce05780649369c8e98b85af9c10b7
Igor Kuryakov <remrtehno@gmail.com>
2025-01-05 21:54:05 +0100

    [253]: iterative variant

--- lc_253_lowest_common_ancestor_of_a_binary_search_tree.py
+++ lc_253_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -7,6 +7,17 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        curr = root
+
+        while curr:
+            if curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            elif curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            else:
+                return curr
+
+    def lowestCommonAncestor2(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
         def dfs(root: TreeNode) -> TreeNode:
             if root.val < p.val and root.val < q.val:
                 return dfs(root.right)
